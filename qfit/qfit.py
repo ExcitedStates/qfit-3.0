Reworked footprint calculation for scaling. Fixed issues with density subtraction for soft clash treatment.

qfit/qfit.py
@@ -192,21 +192,11 @@ class _BaseQFit:
         self._transformer.initialize()
 
     def _subtract_transformer(self, residue, structure):
-        sel_str = f"resi {residue.resi[0]} and chain {residue.chain[0]}"
-        sel_str = f"not ({sel_str})"
-        neighbors = structure.extract(sel_str)
-        mask = (neighbors.coor[:, 1] < -np.inf)
-        for coor in residue.coor:
-            diffs = neighbors.coor - np.array(coor)
-            dists = np.linalg.norm(diffs, axis=1)
-            mask = np.logical_or(mask, dists < self.options.padding)
+        # Select the atoms whose density we are going to subtract:
+        subtract_structure = structure.extract_neighbors(residue,
+                                                         self.options.padding)
 
-        data = {}
-        for attr in neighbors.data:
-            array1 = getattr(neighbors, attr)
-            data[attr] = array1[mask]
-        subtract_structure = Structure(data)
-
+        # Calculate the density that we are going to subtract:
         self._subtransformer = Transformer(
             subtract_structure,
             self._xmap_model2, smax=self._smax, smin=self._smin,
@@ -215,10 +205,16 @@ class _BaseQFit:
         self._subtransformer.initialize()
         self._subtransformer.reset(full=True)
         self._subtransformer.density()
+
+
+        # Set the lowest values in the map to the bulk solvent level:
         model = copy.deepcopy(self._subtransformer.xmap.array)
         model = np.maximum(self.options.bulk_solvent_level, self._subtransformer.xmap.array, model)
-        self._subtransformer.xmap.array = 2*model
+        self._subtransformer.xmap.array = model
+
+        # Subtract the density:
         self.xmap.array -= self._subtransformer.xmap.array
+
 
 
     def _convert(self):
@@ -462,7 +458,7 @@ class QFitRotamericResidue(_BaseQFit):
                 footprint = structure.extract(sel_str)
                 footprint = footprint.extract('record', 'ATOM')
                 scaler.scale(footprint, radius=1)
-            xmap = xmap.extract(residue.coor, padding=5)
+            xmap = xmap.extract(residue.coor, padding=options.padding)
 
         # Check if residue is complete. If not, complete it:
         atoms = residue.name

qfit/qfit_residue.py
@@ -126,7 +126,7 @@ def parse_args():
             help="Include hydrogens during calculations.")
     p.add_argument("-M", "--miosqp", dest="cplex", action="store_false",
             help="Use MIOSQP instead of CPLEX for the QP/MIQP calculations.")
-    p.add_argument("-T","--threshold-selection", dest="bic_threshold", action="store_true",
+    p.add_argument("-T", "--threshold-selection", dest="bic_threshold", action="store_true",
             help="Use BIC to select the most parsimonious MIQP threshold")
 
     # Output options
@@ -245,7 +245,8 @@ def main():
             reso = options.resolution
         if reso is not None:
             radius = 0.5 + reso / 3.0
-        scaler.scale(footprint, radius=args.scale_rmask*radius)
+        #scaler.scale(footprint, radius=args.scale_rmask*radius)
+        scaler.scale(structure, radius=args.scale_rmask*radius)
     xmap = xmap.extract(residue.coor, padding=args.padding)
     ext = '.ccp4'
     if not np.allclose(xmap.origin, 0):