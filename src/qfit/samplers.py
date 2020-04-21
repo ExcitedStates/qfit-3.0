Fix array pointer bug in rotation routines
This is _definitely_ a bug in structure/residue.py, but I have also made this change elsewhere.

Previously:
- zaxis = coor[2] creates a slice-view of the coor array
- The in-place divide acts not just on zaxis, but also on the underlying coordinates of atom[2] in the coor array!
Now:
- We create new arrays by not doing any inplace maths operations

Note that origin is defined as coor[1].copy(). We could equally do this for z-axis: as a more verbose way of fixing this bug.

src/qfit/samplers.py
@@ -250,8 +250,7 @@ class ChiRotator:
         coor = self.residue._coor[selection]
         self._origin = coor[1].copy()
         coor -= self._origin
-        zaxis = coor[2]
-        zaxis /= norm(zaxis)
+        zaxis = coor[2] / norm(coor[2])
         yaxis = coor[0] - np.inner(coor[0], zaxis) * zaxis
         yaxis /= norm(yaxis)
         xaxis = np.cross(yaxis, zaxis)

src/qfit/structure/residue.py
@@ -183,8 +183,7 @@ class _RotamerResidue(_BaseResidue):
         coor -= origin
 
         # Make an orthogonal axis system based on 3 atoms
-        zaxis = coor[2]
-        zaxis /= np.linalg.norm(zaxis)
+        zaxis = coor[2] / np.linalg.norm(coor[2])
         yaxis = coor[0] - np.inner(coor[0], zaxis) * zaxis
         yaxis /= np.linalg.norm(yaxis)
         xaxis = np.cross(yaxis, zaxis)

src/qfit/structure/structure.py
@@ -845,8 +845,7 @@ class _Segment(_BaseStructure):
         origin = system_coor[0].copy()
         system_coor = np.vstack((CA, C, O))
         system_coor -= origin
-        zaxis = system_coor[1]
-        zaxis /= np.linalg.norm(zaxis)
+        zaxis = system_coor[1] / np.linalg.norm(system_coor[1])
         yaxis = system_coor[2] - np.inner(system_coor[2], zaxis) * zaxis
         yaxis /= np.linalg.norm(yaxis)
         xaxis = np.cross(yaxis, zaxis)