0433f24f1966872466470d86ff47bea713f04915
Blake Riley <blake.riley@gmail.com>
2020-05-29 00:22:34 -0400

    Use numpy methods, not nested loops
    This commit speeds up all of qFit by a factor of ~2.3.
    
    Previously, _QPSolver.initialize used nested for-loops to compute inner products.
    Given that this came from a C++ project, I understand why it was there.
    But, Python can't access the same loop-unrolling optimisations of a compiled language, so it's mighty slow... not to mention all the array slicing and access overhead.
    
    Previously, this routine took around 140 s (just to construct an array!). It now takes about 4.6 s.
    
    By default, np.inner will do a sum product over the last axes: so for the 2D matrices we have here, np.inner(A[i,j], B[k,l]) -> C[i,k] . This was precisely the function of the nested for loops.

--- src/qfit/solvers.py
+++ src/qfit/solvers.py
@@ -82,14 +82,8 @@ if CPLEX:
         def initialize(self):
             # Set up the matrices and restraints
             logger.debug(f"Building cvxopt matrix, size: ({self._nconformers},{self._nconformers})")
-            self._quad_obj = cvxopt.matrix(0, (self._nconformers, self._nconformers), tc='d')
-            self._lin_obj = cvxopt.matrix(self._nconformers * [0], tc='d')
-            for i in range(self._nconformers):
-                for j in range(i, self._nconformers):
-                    self._quad_obj[i, j] = np.inner(self._models[i], self._models[j])
-                    # Matrix is symmetric
-                    self._quad_obj[j, i] = self._quad_obj[i, j]
-                self._lin_obj[i] = -np.inner(self._models[i], self._target)
+            self._quad_obj = cvxopt.matrix(np.inner(self._models, self._models), tc='d')
+            self._lin_obj = cvxopt.matrix(-np.inner(self._models, self._target), tc='d')
 
             # lower-equal constraints.
             # Each weight falls in the closed interval [0..1] and its sum is <= 1.
@@ -127,15 +121,8 @@ if CPLEX:
             self.threads = threads
 
         def initialize(self):
-
-            self._quad_obj = np.zeros((self._nconformers, self._nconformers))
-            self._lin_obj = np.zeros(self._nconformers)
-            for i in range(self._nconformers):
-                for j in range(i, self._nconformers):
-                    self._quad_obj[i, j] = np.inner(self._models[i], self._models[j])
-                    # Matrix is symmetric
-                    self._quad_obj[j, i] = self._quad_obj[i, j]
-                self._lin_obj[i] = -np.inner(self._models[i], self._target)
+            self._quad_obj = np.inner(self._models, self._models)
+            self._lin_obj = -np.inner(self._models, self._target)
 
             self.initialized = True
 
