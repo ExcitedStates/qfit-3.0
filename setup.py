33a7930fcf207e2fc2c50e1c6c1640362e8bd4ba
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2020-08-06 15:44:47 -0400

    adding qfit_final_refine_cryoem.sh to setup.py

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ def main():
               ]},
           scripts=[
               'scripts/post/qfit_final_refine_xray.sh',
+              'scripts/post/qfit_final_refine_cryoem.sh',
               'scripts/post/find_largest_ligand.py',
               'scripts/post/find_altlocs_near_ligand.py',
               'scripts/post/qfit_RMSF.py',
