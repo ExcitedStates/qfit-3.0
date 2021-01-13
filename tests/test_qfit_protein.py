de651d3f42e32f30b71a16179bfe0e2a1b3e3bbb
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2021-01-13 08:40:07 -0800

    Update test_qfit_protein.py

--- tests/test_qfit_protein.py
+++ tests/test_qfit_protein.py
@@ -35,7 +35,7 @@ class TestQFitProtein:
             "../example/3K0N.mtz",  # mapfile, using relative directory from tests/
             "../example/3K0N.pdb",  # structurefile, using relative directory from tests/
             "-l", "2FOFCWT,PH2FOFCWT"
-            #"--directory", f"{os.environ['QFIT_OUTPUT_DIR']}",
+            "--directory", f"{os.environ['QFIT_OUTPUT_DIR']}",
         ]
 
         # Add options to reduce computational load
