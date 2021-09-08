ced6ba8829fd9fb0378b02a17968c5e6fa022926
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2021-09-08 14:07:40 -0700

    update numpy version to 1.20
    Due to issue: 
    ImportError: numpy.core.multiarray failed to import 
    RuntimeError: module compiled against API version 0xe but this version of numpy is 0xd

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def main():
         'setuptools_scm',
     ]
     install_requires = [
-        'numpy>=1.17',
+        'numpy>=1.20',
         'scipy>=1.00',
         'pyparsing>=2.2.0',
         'tqdm>=4.0.0',
