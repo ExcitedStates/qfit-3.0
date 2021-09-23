3b4a6c81b0f08a30fe09225b87d34d232ebee902
Stephanie Wankowicz <stephanie.wankowicz@ucsf.edu>
2021-09-23 13:01:39 -0700

    adding qfit_protein for scale/cryst

--- src/qfit/qfit_protein.py
+++ src/qfit/qfit_protein.py
@@ -306,7 +306,10 @@ class QFitProtein:
         multiconformer = multiconformer.combine(hetatms)
         fname = os.path.join(self.options.directory,
                              "multiconformer_model.pdb")
-        multiconformer.tofile(fname)
+        if self.structure.scale or self.structure.cryst_info:
+            multiconformer.tofile(fname, self.structure.scale, self.structure.cryst_info)
+        else:
+            multiconformer.tofile(fname)
         return multiconformer
 
     def _run_qfit_segment(self, multiconformer):
@@ -321,7 +324,10 @@ class QFitProtein:
         multiconformer = qfit()
         fname = os.path.join(self.options.directory,
                              self.pdb + "multiconformer_model2.pdb")
-        multiconformer.tofile(fname)
+        if self.structure.scale or self.structure.cryst_info:
+            multiconformer.tofile(fname, self.structure.scale, self.structure.cryst_info)
+        else:
+            multiconformer.tofile(fname)
         return multiconformer
 
     @staticmethod
