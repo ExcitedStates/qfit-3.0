acf1c5ea65e1b7b9baef0436b37f4c4cd22c8cbf
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-16 17:53:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ def main():
                   'qfit_residue = qfit.qfit_residue:main',
                   'qfit_ligand  = qfit.qfit_ligand:main',
                   'qfit_covalent_ligand = qfit.qfit_covalent_ligand:main',
+                  'qfit_water = qfit.run_qfit_water:main',
                   'qfit_segment = qfit.qfit_segment:main',
                   'qfit_prep_map = qfit.qfit_prep_map:main',
                   'qfit_density = qfit.qfit_density:main',
