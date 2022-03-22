66e8d5622d0f4fb0e63ff2e8581aa953b653fd85
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-22 16:59:51 -0700

    clean up script
    adding alpha carbon and sidechain options

--- scripts/post/b_factor.py
+++ scripts/post/b_factor.py
@@ -12,81 +12,34 @@ def build_argparser():
     p = ArgumentParser(description=__doc__)
     p.add_argument("structure", type=str,
                    help="PDB-file containing structure.")
-   # Output options
+    p.add_argument("--ca", dest="ca", action="store_true",
+            help="Return B factor for alpha carbons only")
+    p.add_argument("--sidechain", dest='sc', action="store_true", 
+                  help="Return B factor for sidechain atoms only")
     p.add_argument("--pdb", help="Name of the input PDB.")
-
     return p
 
-class Bfactor_options(QFitRotamericResidueOptions):
-    def __init__(self):
-        super().__init__()
-        self.pdb = None
-        self.ca = None
-
-class B_factor():
-    def __init__(self, structure, options):
-        self.structure = structure 
-        self.options = options 
-    
-    def run(self):
-        if not self.options.pdb == None:
-            self.pdb = self.options.pdb+'_'
-        else:
-            self.pdb = ''
-        self.get_bfactors()
-
-    def get_bfactors(self):
-        B_factor = pd.DataFrame()
-        atom_name = []
-        chain_ser = []
-        residue_name = []
-        b_factor = []
-        residue_num = []
-        model_number = []
-        select = self.structure.extract('record', 'ATOM', '==')
-        select = select.extract('e', 'H', '!=')
-        if not self.options.ca == None:
-           select = select.extract('name', 'CA', '==')
-        n = 0
-        for chain in np.unique(select.chain):
-            select2 = select.extract('chain', chain, '==')
-            residues = set(list(select2.resi))
-            residue_ids = []
-            for i in residues:
-                tmp_i = str(i)
-                if ':' in tmp_i:
-                    resi = int(tmp_i.split(':')[1][1:])
-                else:
-                    resi = tmp_i
-                residue_ids.append(resi)
-        
-        n=1
-        for id in residue_ids:
-            res_tmp = select2.extract('resi', int(id), '==') #this is seperating each residues
-            #is this going to give us the alternative coordinate for everything?
-            resn_name = (np.array2string(np.unique(res_tmp.resi)), np.array2string(np.unique(res_tmp.resn)),np.array2string(np.unique(res_tmp.chain)))
-            b_factor = res_tmp.b
-            B_factor.loc[n,'resseq'] = resn_name[0]
-            B_factor.loc[n,'AA'] = resn_name[1]
-            B_factor.loc[n,'Chain'] = resn_name[2]
-            B_factor.loc[n,'Max_Bfactor'] = np.amax(b_factor)
-            B_factor.loc[n, 'Average_Bfactor'] = np.average(b_factor)
-            n+=1
-        B_factor.to_csv(self.pdb + '_B_factors.csv', index=False)
+def get_bfactor(structure, pdb, ca = None, sidechain = None):
+    bfactors = []
+    select = structure.extract('record', 'ATOM', '==')
+    select = select.extract('e', 'H', '!=')
+    if not ca == None:
+       select = select.extract('name', 'CA', '==')
+    if not sidechain == None:
+        select = select.extract('name', (['N', 'CA', 'C', 'O']), '!=')
+    for c in np.unique(select.chain):
+        for r in np.unique(select.extract('chain', c, '==').resi:
+            b_factor = np.average(select.extract(f'resi {r} and chain {c}').b) / np.average(select.extract(f'resi {r} and chain {c}').q)
+            bfactors.append(tuple((pdb, np.array2string(r), np.array2string(select.extract(f'resi {r} and chain {c}').resn[0]), np.array2string(np.unique(c), b_factor)))
+     B_factor = pd.DataFrame(bfactors, columns =['PDB', 'resi', 'resn', 'chain', 'b_factor'])
+     return B_factor   
+     
 
 def main():
-    print(sys.path)
     p = build_argparser()
     args = p.parse_args()
-
-    structure = Structure.fromfile(args.structure).reorder()
-    print(structure)
-    B_options = Bfactor_options()
-    B_options.apply_command_args(args)
-
-    b_factor = B_factor(structure, B_options)
-    b_fin = b_factor.run()
-
+    B_factor = get_bfactor(args.structure, args.pdb, args.ca, args.sc)
+    B_factor.to_csv(args.pdb + '_B_factors.csv', index=False)                        
 
 if __name__ == '__main__':
     main()
