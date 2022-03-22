118976bb4b2d3fa0aa30756cfe54ffc33683eb66
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-22 16:42:28 -0700

    remove imports

--- scripts/post/relabel_chain.py
+++ scripts/post/relabel_chain.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import argparse
-import logging
-import copy
 import os
-import sys
 from string import ascii_uppercase
 from qfit.structure import Structure
 
