f8df82af91bf6a09ce55fd5a62c16ac113ac36c3
Blake Riley <blake.riley@gmail.com>
2022-04-23 03:02:53 -0400

    Check file extensions and file order with argparse.Action instead

--- src/qfit/qfit_protein.py
+++ src/qfit/qfit_protein.py
@@ -9,7 +9,7 @@ import os
 import sys
 import time
 import argparse
-from .custom_argparsers import ToggleActionFlag, CustomHelpFormatter
+from .custom_argparsers import ToggleActionFlag, CustomHelpFormatter, ValidateMapFileArgument, ValidateStructureFileArgument
 import logging
 import traceback
 from .logtools import setup_logging, log_run_info, poolworker_setup_logging, QueueListener
@@ -25,22 +25,15 @@ def build_argparser():
     p = argparse.ArgumentParser(formatter_class=CustomHelpFormatter,
                                 description=__doc__)
 
-    #file extension check for map and structure. 
-    def file_choices(choices,fname):
-        ext = os.path.splitext(fname)[1][1:]
-        if ext not in choices:
-           p.error(f"File doesn't end with one of {choices} Please make sure the map/model are in the correct order: map, model.")
-        return fname
-
     p.add_argument("map",
                    help="Density map in CCP4 or MRC format, or an MTZ file "
                         "containing reflections and phases. For MTZ files "
-                        "use the --label options to specify columns to read."                        
+                        "use the --label options to specify columns to read. "
                         "For CCP4 files, use the -r to specify resolution.",
-                        type=lambda s:file_choices(("ccp4","mtz","mrc", "map"),s))
+                   type=str, action=ValidateMapFileArgument)
     p.add_argument("structure",
-                   help="PDB-file containing structure.", 
-                   type=lambda s:file_choices(("pdb"),s))
+                   help="PDB-file containing structure.",
+                   type=str, action=ValidateStructureFileArgument)
 
     # Map input options
     p.add_argument("-l", "--label", default="FWT,PHWT",
@@ -473,8 +466,6 @@ def main():
     #   (When args==None, argparse will default to sys.argv[1:])
     p = build_argparser()
     args = p.parse_args(args=None)
-    #function to ensure map/model are correct
-    check_map_model_file()
 
     try:
         os.mkdir(args.directory)
