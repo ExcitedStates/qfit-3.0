535e4e0449a10092390aa3f308beb60f383bcc9a
natechols <nathaniel.echols@gmail.com>
2022-05-11 15:06:45 -0700

    add unit tests for MTZ reader, using cctbx mocked inputs #202

---
+++ tests/unit/base_test_case.py
@@ -0,0 +1,28 @@
+"""
+Core utilities for unit-testing with mocked data
+"""
+import tempfile
+import unittest
+import os.path as op
+
+class UnitBase(unittest.TestCase):
+    DATA = op.join(op.dirname(op.dirname(__file__)), "data")
+    TINY_PDB = op.join(DATA, "phe113_fake_uc.pdb")
+
+    def make_tiny_fmodel_mtz(self):
+        """
+        Use 'phenix.fmodel' to generate Fcalc map coefficients in MTZ format
+        """
+        mtz_out = tempfile.NamedTemporaryFile(suffix="-fmodel.mtz").name
+        from mmtbx.command_line import fmodel
+        from libtbx.utils import null_out
+        args = [
+            self.TINY_PDB,
+            "high_resolution=1.39",
+            "r_free_flags_fraction=0.1",
+            "output.label=FWT",
+            "output.file_name={}".format(mtz_out)
+        ]
+        fmodel.run(args=args, log=null_out())
+        print(mtz_out)
+        return mtz_out
---
+++ tests/unit/test_volume.py
@@ -0,0 +1,24 @@
+import unittest
+import os.path as op
+
+import pytest
+
+from qfit.volume import XMap
+
+from .base_test_case import UnitBase
+
+class TestVolume(UnitBase):
+
+    def test_xmap_from_mtz(self):
+        MTZ = self.make_tiny_fmodel_mtz()
+        xmap = XMap.fromfile(MTZ, label="FWT,PHIFWT")
+        assert xmap.resolution.high == pytest.approx(1.3922, abs=0.001)
+        assert xmap.resolution.low == pytest.approx(9.3704, abs=0.001)
+        assert str(xmap.unit_cell) == "UnitCell(a=8.000000, b=12.000000, c=15.000000, alpha=90.000000, beta=90.000000, gamma=90.000000)"
+        assert xmap.array.size == 28160
+        assert xmap.hkl.size == 354*3
+        assert list(xmap.origin) == [0., 0., 0.]
+        assert xmap.array.shape == (40, 32, 22)
+        #assert list(xmap.array[0:5]) == ""
+        assert xmap.array[0][0][0] == pytest.approx(-0.4325, abs=0.0001)
+        assert xmap.array[20][16][11] == pytest.approx(0.1438, abs=0.0001)
