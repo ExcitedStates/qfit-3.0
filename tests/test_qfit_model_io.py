6c4aaaf6c17c584c735e68ae553465d7f79cb59b
Ashraya Ravikumar <ashrayar@gmail.com>
2022-07-16 19:52:29 -0700

    Created a new test script (test_qfit_model_io.py) which checks read/write functionality of qFit for model files. It checks the following:
    1. Read and write model file with lower case and upper case chain name (ex: j and J) and negative residue numbers
    2. Read model file with 4-digit residue numbers
    3. Read model file and extract residues which are marked as HETATM in the middle of ATOM records since they are modified (ex: TPO)
    4. Read mmCIF model file and extract residues from chain with name containing two characters (ex: HA). This test is currently commented and can be used only after mmCIF file parsing has been implemented in qFit
    
    All the relevant model and map files have been added to the example folder

---
+++ tests/test_qfit_model_io.py
@@ -0,0 +1,237 @@
+import pytest
+import os
+import logging
+import multiprocessing as mp
+
+from qfit.qfit_protein import (
+    QFitOptions,
+    build_argparser,
+    prepare_qfit_protein,
+)
+from qfit.logtools import (
+    setup_logging,
+    log_run_info,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+def setup_module(module):
+    # Here, we add compatibility for multiprocessing coverage reports.
+    # via: https://pytest-cov.readthedocs.io/en/latest/subprocess-support.html#if-you-use-multiprocessing-pool
+    try:
+        from pytest_cov.embed import cleanup_on_sigterm
+    except ImportError:
+        pass
+    else:
+        cleanup_on_sigterm()
+
+
+class TestQFitProtein:
+    def mock_main(self):
+        # Prepare args
+        args = [
+            "./example/1fnt_phases.mtz",  # mapfile, using relative directory from tests/
+            "./example/1fnt.pdb",  # structurefile, using relative directory from tests/
+            "-l",
+            "FWT,PHWT",
+        ]
+
+        # Add options to reduce computational load
+        args.extend(
+            [
+                "--backbone-amplitude",
+                "0.10",  # default: 0.30
+                "--rotamer-neighborhood",
+                "30",  # default: 60
+            ]
+        )
+
+        # Collect and act on arguments
+        p = build_argparser()
+        args = p.parse_args(args=args)
+        try:
+            os.mkdir(args.directory)
+        except OSError:
+            pass
+
+        # Apply the arguments to options
+        options = QFitOptions()
+        options.apply_command_args(args)
+        options.debug = True  # For debugging in tests
+
+        # Setup logger
+        setup_logging(options=options)
+        log_run_info(options, logger)
+
+        # Build a QFitProtein job
+        qfit = prepare_qfit_protein(options)
+
+        # Reach into QFitProtein job,
+        # Test 1: chain J and j present in structure. Only J chain has negative residues
+        chain = "J"
+        qfit.structure = qfit.structure.extract("chain", chain, "==")
+        qfit.structure = qfit.structure.extract("resi", (-7, -5, -3, -2), "==")
+        assert (len(list(qfit.structure.single_conformer_residues))) == 4
+        # output the extracted residues to file
+        qfit.structure.tofile("./example/j_neg_residues.pdb")
+
+        # Read the written file again and check
+        args = [
+            "./example/1fnt_phases.mtz",
+            "./example/j_neg_residues.pdb",
+            "-l",
+            "FWT,PHWT",
+        ]
+
+        args.extend(
+            [
+                "--backbone-amplitude",
+                "0.10",  # default: 0.30
+                "--rotamer-neighborhood",
+                "30",  # default: 60
+            ]
+        )
+
+        # Collect and act on arguments
+        p = build_argparser()
+        args = p.parse_args(args=args)
+        try:
+            os.mkdir(args.directory)
+        except OSError:
+            pass
+
+        # Apply the arguments to options
+        options = QFitOptions()
+        options.apply_command_args(args)
+        options.debug = True  # For debugging in tests
+
+        qfit = prepare_qfit_protein(options)
+        qfit.structure = qfit.structure.extract("resi", (-3, -2), "==")
+        assert (len(list(qfit.structure.single_conformer_residues))) == 2
+
+        # Test 2: Check if 4 digit residues are read correctly
+        args = [
+            "./example/4e3y_phases.mtz",  # mapfile, using relative directory from tests/
+            "./example/4e3y.pdb",  # structurefile, using relative directory from tests/
+            "-l",
+            "FWT,PHWT",
+        ]
+
+        # Add options to reduce computational load
+        args.extend(
+            [
+                "--backbone-amplitude",
+                "0.10",  # default: 0.30
+                "--rotamer-neighborhood",
+                "30",  # default: 60
+            ]
+        )
+
+        # Collect and act on arguments
+        p = build_argparser()
+        args = p.parse_args(args=args)
+        try:
+            os.mkdir(args.directory)
+        except OSError:
+            pass
+
+        # Apply the arguments to options
+        options = QFitOptions()
+        options.apply_command_args(args)
+        options.debug = True  # For debugging in tests
+
+        qfit = prepare_qfit_protein(options)
+
+        # Reach into QFitProtein job,
+        # simplify to only run on two residues (reduce computational load)
+        qfit.structure = qfit.structure.extract("resi", (1024, 1025), "==")
+        assert (len(list(qfit.structure.single_conformer_residues))) == 2
+
+        # Test 3: Read PDB file with HETATM record appearing in between ATOM records
+
+        args = [
+            "./example/5orl_phases.mtz",  # mapfile, using relative directory from tests/
+            "./example/5orl.pdb",  # structurefile, using relative directory from tests/
+            "-l",
+            "FWT,PHWT",
+        ]
+
+        # Add options to reduce computational load
+        args.extend(
+            [
+                "--backbone-amplitude",
+                "0.10",  # default: 0.30
+                "--rotamer-neighborhood",
+                "30",  # default: 60
+            ]
+        )
+
+        # Collect and act on arguments
+        p = build_argparser()
+        args = p.parse_args(args=args)
+        try:
+            os.mkdir(args.directory)
+        except OSError:
+            pass
+
+        # Apply the arguments to options
+        options = QFitOptions()
+        options.apply_command_args(args)
+        options.debug = True  # For debugging in tests
+
+        qfit = prepare_qfit_protein(options)
+
+        # Residue 288 is marked as HETATM in the PDB file since it is a modified residue
+        qfit.structure = qfit.structure.extract("resi", (287, 288, 289), "==")
+        assert (np.unique(qfit.structure.resi)[1]) == 288
+
+        # Test 4: Read mmCIF file and map file of cryoEM structure. To be uncommented after cctbx integration is complete
+        """
+        args = [
+            "./example/7o9m.map",  # mapfile, using relative directory from tests/
+            "./example/7o9m.pdb",  # structurefile, using relative directory from tests/
+            #"-l", "FWT,PHWT",
+        ]
+
+        # Add options to reduce computational load
+        args.extend([
+            "--backbone-amplitude", "0.10",  # default: 0.30
+            "--rotamer-neighborhood", "30",  # default: 60
+        ])
+
+        # Collect and act on arguments
+        p = build_argparser()
+        args = p.parse_args(args=args)
+        try:
+            os.mkdir(args.directory)
+        except OSError:
+            pass
+
+        # Apply the arguments to options
+        options = QFitOptions()
+        options.apply_command_args(args)
+        options.debug = True  # For debugging in tests
+
+        qfit = prepare_qfit_protein(options)
+        chain='FA'
+        # Reach into QFitProtein job,
+        # simplify to only run on two residues (reduce computational load)
+        qfit.structure = qfit.structure.extract('chain', chain, '==')
+        qfit.structure = qfit.structure.extract('resi',(130,134),"==")
+        assert(len(list(qfit.structure.single_conformer_residues))) == 2
+        """
+
+        return qfit
+
+    def test_run_qfit_residue_parallel(self):
+        qfit = self.mock_main()
+
+        # Run qfit object
+        multiconformer = qfit._run_qfit_residue_parallel()
+        mconformer_list = list(multiconformer.residues)
+        print(mconformer_list)  # If we fail, this gets printed.
+
+        # Following statement needs to be modified
+        assert len(mconformer_list) == 5  # Expect: 3*Ser99, 2*Phe113
