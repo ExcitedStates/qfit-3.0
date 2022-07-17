fbf583d7124cf281d8800ba694c5ac85c9f4dc43
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-07-17 10:07:42 -0700

    fix labels for mtz

--- tests/test_SG_I422.py
+++ tests/test_SG_I422.py
@@ -34,7 +34,7 @@ class TestQFitProtein:
         args = [
             "./example/5gge.mtz",  # mapfile, using relative directory from tests/
             "./example/5GGE.pdb",  # structurefile, using relative directory from tests/
-            "-l", "2FOFCWT,PH2FOFCWT",
+            "-l", "FP,SIGFP",
         ]
 
         # Add options to reduce computational load
