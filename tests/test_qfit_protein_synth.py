363d53fe9919edd71721cafa6bb80fa147ae865d
natechols <nathaniel.echols@gmail.com>
2022-07-27 22:43:46 -0700

    smaller/faster synthetic test

--- tests/test_qfit_protein_synth.py
+++ tests/test_qfit_protein_synth.py
@@ -19,8 +19,10 @@ from qfit.structure import Structure
 
 class TestQfitProteinSyntheticData(unittest.TestCase):
     DATA = op.join(op.dirname(__file__), "data")
-    PDB_MULTI = op.join(DATA, "gnnafns_multiconf.pdb")
-    PDB_SINGLE = op.join(DATA, "gnnafns_single.pdb")
+    PDB_7MER_MULTI = op.join(DATA, "gnnafns_multiconf.pdb")
+    PDB_7MER_SINGLE = op.join(DATA, "gnnafns_single.pdb")
+    PDB_3MER_MULTI = op.join(DATA, "afa_multiconf.pdb")
+    PDB_3MER_SINGLE = op.join(DATA, "afa_single.pdb")
     # thd default is deliberately chosen to actually pass the test
     RANDOM_SEED = int(os.environ.get("QFIT_RANDOM_SEED", 7))
     D_MIN = float(os.environ.get("QFIT_FMODEL_DMIN", 1.3))
@@ -73,8 +75,11 @@ class TestQfitProteinSyntheticData(unittest.TestCase):
                 print(e)
         return rotamers
 
-    def _run_qfit_cli(self, pdb_file_multi, pdb_file_single):
-        self._create_fmodel(pdb_file_multi)
+    def _run_qfit_cli(self,
+                      pdb_file_multi,
+                      pdb_file_single,
+                      high_resolution=D_MIN):
+        self._create_fmodel(pdb_file_multi, high_resolution=high_resolution)
         qfit_args = [
             "qfit_protein",
             "fmodel.mtz",
@@ -102,11 +107,13 @@ class TestQfitProteinSyntheticData(unittest.TestCase):
         assert rotamers_in[3] - rotamers_out[3] == set()
         assert rotamers_in[2] - rotamers_out[2] == set()
 
-    def _validate_new_fmodel(self):
+    def _validate_new_fmodel(self, high_resolution=D_MIN):
         from iotbx.file_reader import any_file
         from scitbx.array_family import flex
         mtz_new = "fmodel_out.mtz"
-        self._create_fmodel("multiconformer_model2.pdb", output_file=mtz_new)
+        self._create_fmodel("multiconformer_model2.pdb",
+                            output_file=mtz_new,
+                            high_resolution=high_resolution)
         fmodel_1 = any_file("fmodel.mtz")
         fmodel_2 = any_file("fmodel_out.mtz")
         array1 = fmodel_1.file_object.as_miller_arrays()[0].data()
@@ -116,21 +123,58 @@ class TestQfitProteinSyntheticData(unittest.TestCase):
         assert lc.coefficient() >= 0.95
 
     def test_cli_7mer_peptide_p21(self):
-        self._run_qfit_cli(self.PDB_MULTI, self.PDB_SINGLE)
-        self._validate_7mer_confs(self.PDB_MULTI)
+        self._run_qfit_cli(self.PDB_7MER_MULTI, self.PDB_7MER_SINGLE)
+        self._validate_7mer_confs(self.PDB_7MER_MULTI)
         self._validate_new_fmodel()
 
-    def test_cli_7mer_peptide_p1(self):
+    def _swap_symm(self, space_group_symbol, unit_cell, pdb_multi, pdb_single):
         from cctbx.crystal import symmetry
         new_symm = symmetry(
-            space_group_symbol="P1",
-            unit_cell=(30, 10, 15, 90, 105, 90))
-        s1 = Structure.fromfile(self.PDB_MULTI)
+            space_group_symbol=space_group_symbol,
+            unit_cell=unit_cell)
+        s1 = Structure.fromfile(pdb_multi)
         s1.crystal_symmetry = new_symm
         s1.tofile("multi_p1.pdb")
-        s2 = Structure.fromfile(self.PDB_SINGLE)
+        s2 = Structure.fromfile(pdb_single)
         s2.crystal_symmetry = new_symm
         s2.tofile("single_p1.pdb")
+
+    def test_cli_7mer_peptide_p1(self):
+        self._swap_symm(
+            space_group_symbol="P1",
+            unit_cell=(30, 10, 15, 90, 105, 90),
+            pdb_multi=self.PDB_7MER_MULTI,
+            pdb_single=self.PDB_7MER_SINGLE)
         self._run_qfit_cli("multi_p1.pdb", "single_p1.pdb")
         self._validate_7mer_confs("multi_p1.pdb")
         self._validate_new_fmodel()
+
+    def _validate_3mer_confs(self, pdb_file_multi):
+        rotamers_in = self._get_model_rotamers(pdb_file_multi)
+        rotamers_out = self._get_model_rotamers("multiconformer_model2.pdb")
+        # Phe2 should have two rotamers, but this may occasionally appear as
+        # three due to the ring flips, and we can't depend on which orientation
+        # the ring ends up in
+        assert (-177, 80) in rotamers_out[2]  # this doesn't flip???
+        assert (-65, -85) in rotamers_out[2] or (-65, 85) in rotamers_out[2]
+
+    def test_cli_phe_3mer_p21(self):
+        d_min = 1.5
+        self._run_qfit_cli(self.PDB_3MER_MULTI,
+                           self.PDB_3MER_SINGLE,
+                           high_resolution=d_min)
+        self._validate_3mer_confs(self.PDB_3MER_MULTI)
+        self._validate_new_fmodel(high_resolution=d_min)
+
+    def test_cli_phe_3mer_p1(self):
+        d_min = 1.5
+        self._swap_symm(
+            space_group_symbol="P1",
+            unit_cell=(12, 6, 10, 90, 105, 90),
+            pdb_multi=self.PDB_3MER_MULTI,
+            pdb_single=self.PDB_3MER_SINGLE)
+        self._run_qfit_cli("multi_p1.pdb",
+                           "single_p1.pdb",
+                           high_resolution=d_min)
+        self._validate_3mer_confs("multi_p1.pdb")
+        self._validate_new_fmodel(high_resolution=d_min)
