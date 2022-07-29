962430f254b53fd411707ae0791df2d67a1d74f4
natechols <nathaniel.echols@gmail.com>
2022-07-28 20:15:32 -0700

    more fast synthetic integration tests

--- tests/test_qfit_protein_synth.py
+++ tests/test_qfit_protein_synth.py
@@ -1,8 +1,8 @@
 """
-Relatively fast integration test of qfit_protein using synthetic data for
-a small peptide with several alternate conformers.  VERY unstable without
-setting a pre-determined random seed for qfit_protein; future work should
-investigate the sensitivity.
+Relatively fast integration tests of qfit_protein using synthetic data for
+a variety of small peptide with several alternate conformers.  VERY unstable
+without setting a pre-determined random seed for qfit_protein; future work
+should investigate the sensitivity.
 """
 
 from collections import defaultdict
@@ -19,13 +19,9 @@ from qfit.structure import Structure
 
 class TestQfitProteinSyntheticData(unittest.TestCase):
     DATA = op.join(op.dirname(__file__), "data")
-    PDB_7MER_MULTI = op.join(DATA, "gnnafns_multiconf.pdb")
-    PDB_7MER_SINGLE = op.join(DATA, "gnnafns_single.pdb")
-    PDB_3MER_MULTI = op.join(DATA, "afa_multiconf.pdb")
-    PDB_3MER_SINGLE = op.join(DATA, "afa_single.pdb")
-    # thd default is deliberately chosen to actually pass the test
+    # the default is specifically chosen to make the 7-mer test pass
     RANDOM_SEED = int(os.environ.get("QFIT_RANDOM_SEED", 7))
-    D_MIN = float(os.environ.get("QFIT_FMODEL_DMIN", 1.3))
+    CHI_RADIUS = 10
 
     @pytest.fixture(autouse=True)
     def run_before_and_after_tests(self):
@@ -36,41 +32,50 @@ class TestQfitProteinSyntheticData(unittest.TestCase):
         yield
         os.chdir(cwd)
 
+    def _get_file_path(self, base_name):
+        return op.join(self.DATA, base_name)
+
+    def _get_start_models(self, peptide_name):
+        return (self._get_file_path(f"{peptide_name}_multiconf.pdb"),
+                self._get_file_path(f"{peptide_name}_single.pdb"))
+
     def _create_fmodel(self,
                        pdb_file_name,
-                       high_resolution=D_MIN,
+                       high_resolution,
                        output_file="fmodel.mtz"):
         from mmtbx.command_line import fmodel
         from libtbx.utils import null_out
         fmodel_args = [
             pdb_file_name,
-            f"random_seed={self.RANDOM_SEED}",
             f"high_resolution={high_resolution}",
             "r_free_flags_fraction=0.1",
             "output.label=FWT",
             f"output.file_name={output_file}"
         ]
+        pdb_link = output_file.replace(".mtz", "_in.pdb")
+        os.symlink(pdb_file_name, pdb_link)
         fmodel.run(args=fmodel_args, log=null_out())
         return output_file
 
-    def _get_rotamer(self, residue):
+    def _get_rotamer(self, residue, chi_radius=CHI_RADIUS):
+        # FIXME this is awful, we should replace it with something like
+        # mmtbx.rotalyze but I don't have the necessary library data
         if len(residue.rotamers) == 0:
             return None
-        # XXX this seems like it should be tighter at 1.3A
-        TOLERANCE = 15
         chis = [residue.get_chi(i+1) for i in range(len(residue.rotamers[0]))]
         for rotamer in residue.rotamers:
             delta_chi = [abs(a-b) for a, b in zip(chis, rotamer)]
-            if all([x < TOLERANCE or x > 360-TOLERANCE for x in delta_chi]):
+            if all([x < chi_radius or x > 360-chi_radius for x in delta_chi]):
                 return tuple(rotamer)
         raise ValueError(f"Can't find a rotamer for residue {residue}")
 
-    def _get_model_rotamers(self, file_name):
+    def _get_model_rotamers(self, file_name, chi_radius=CHI_RADIUS):
         s = Structure.fromfile(file_name)
         rotamers = defaultdict(set)
         for residue in s.residues:
             try:
-                rotamers[residue.resi[0]].add(self._get_rotamer(residue))
+                rot = self._get_rotamer(residue, chi_radius=chi_radius)
+                rotamers[residue.resi[0]].add(rot)
             except (IndexError, ValueError) as e:
                 print(e)
         return rotamers
@@ -78,8 +83,9 @@ class TestQfitProteinSyntheticData(unittest.TestCase):
     def _run_qfit_cli(self,
                       pdb_file_multi,
                       pdb_file_single,
-                      high_resolution=D_MIN):
+                      high_resolution):
         self._create_fmodel(pdb_file_multi, high_resolution=high_resolution)
+        os.symlink(pdb_file_single, "single.pdb")
         qfit_args = [
             "qfit_protein",
             "fmodel.mtz",
@@ -92,22 +98,9 @@ class TestQfitProteinSyntheticData(unittest.TestCase):
         print(" ".join(qfit_args))
         subprocess.check_call(qfit_args)
 
-    def _validate_7mer_confs(self, pdb_file_multi):
-        rotamers_in = self._get_model_rotamers(pdb_file_multi)
-        rotamers_out = self._get_model_rotamers("multiconformer_model2.pdb")
-        # Phe5 should have two rotamers, but this may occasionally appear as
-        # three due to the ring flips, and we can't depend on which orientation
-        # the ring ends up in
-        assert (-177, 80) in rotamers_out[5]  # this doesn't flip???
-        assert (-65, -85) in rotamers_out[5] or (-65, 85) in rotamers_out[5]
-        # Asn are also awkward because of flips
-        assert len(rotamers_out[3]) >= 2
-        assert len(rotamers_out[6]) >= 2
-        # these are all of the alt confs present in the fmodel structure
-        assert rotamers_in[3] - rotamers_out[3] == set()
-        assert rotamers_in[2] - rotamers_out[2] == set()
-
-    def _validate_new_fmodel(self, high_resolution=D_MIN):
+    def _validate_new_fmodel(self,
+                             high_resolution,
+                             expected_correlation=0.99):
         from iotbx.file_reader import any_file
         from scitbx.array_family import flex
         mtz_new = "fmodel_out.mtz"
@@ -119,62 +112,160 @@ class TestQfitProteinSyntheticData(unittest.TestCase):
         array1 = fmodel_1.file_object.as_miller_arrays()[0].data()
         array2 = fmodel_2.file_object.as_miller_arrays()[0].data()
         lc = flex.linear_correlation(flex.abs(array1), flex.abs(array2))
-        # correlation of the single-conf fmodel is 0.922
-        assert lc.coefficient() >= 0.95
+        # correlation of the single-conf 7-mer fmodel is 0.922
+        assert lc.coefficient() >= expected_correlation
 
-    def test_cli_7mer_peptide_p21(self):
-        self._run_qfit_cli(self.PDB_7MER_MULTI, self.PDB_7MER_SINGLE)
-        self._validate_7mer_confs(self.PDB_7MER_MULTI)
-        self._validate_new_fmodel()
-
-    def _swap_symm(self, space_group_symbol, unit_cell, pdb_multi, pdb_single):
+    def _replace_symmetry(self,
+                          space_group_symbol,
+                          unit_cell,
+                          pdb_multi,
+                          pdb_single):
         from cctbx.crystal import symmetry
         new_symm = symmetry(
             space_group_symbol=space_group_symbol,
             unit_cell=unit_cell)
         s1 = Structure.fromfile(pdb_multi)
         s1.crystal_symmetry = new_symm
-        s1.tofile("multi_p1.pdb")
+        s1.tofile("multi_newsymm.pdb")
         s2 = Structure.fromfile(pdb_single)
         s2.crystal_symmetry = new_symm
-        s2.tofile("single_p1.pdb")
+        s2.tofile("single_newsymm.pdb")
+        return ("multi_newsymm.pdb", "single_newsymm.pdb")
 
-    def test_cli_7mer_peptide_p1(self):
-        self._swap_symm(
-            space_group_symbol="P1",
-            unit_cell=(30, 10, 15, 90, 105, 90),
-            pdb_multi=self.PDB_7MER_MULTI,
-            pdb_single=self.PDB_7MER_SINGLE)
-        self._run_qfit_cli("multi_p1.pdb", "single_p1.pdb")
-        self._validate_7mer_confs("multi_p1.pdb")
-        self._validate_new_fmodel()
+    def _run_and_validate_identical_rotamers(self,
+                                             pdb_multi,
+                                             pdb_single,
+                                             d_min,
+                                             chi_radius=CHI_RADIUS):
+        self._run_qfit_cli(pdb_multi, pdb_single, high_resolution=d_min)
+        self._validate_new_fmodel(high_resolution=d_min)
+        rotamers_in = self._get_model_rotamers(pdb_multi, chi_radius)
+        rotamers_out = self._get_model_rotamers("multiconformer_model2.pdb",
+                                                chi_radius)
+        assert rotamers_in[2] == rotamers_out[2]
+        return rotamers_out[2]
 
-    def _validate_3mer_confs(self, pdb_file_multi):
+    def _run_3mer_and_validate_identical_rotamers(self,
+                                                  peptide_name,
+                                                  d_min,
+                                                  chi_radius=CHI_RADIUS):
+        (pdb_multi, pdb_single) = self._get_start_models(peptide_name)
+        return self._run_and_validate_identical_rotamers(pdb_multi, pdb_single, d_min, chi_radius)
+
+    def test_qfit_protein_3mer_arg_p21(self):
+        """Build an Arg residue with two conformers"""
+        self._run_3mer_and_validate_identical_rotamers("ARA", d_min=1.0, chi_radius=5)
+
+    def test_qfit_protein_3mer_lys_p21(self):
+        """Build a Lys residue with three rotameric conformations"""
+        rotamers = self._run_3mer_and_validate_identical_rotamers("AKA", d_min=1.2, chi_radius=15)
+        assert len(rotamers) == 3  # just to be certain
+
+    def test_qfit_protein_3mer_ser_p21(self):
+        """Build a Ser residue with two rotamers at moderate resolution"""
+        self._run_3mer_and_validate_identical_rotamers("ASA", 1.7, chi_radius=15)
+
+    def test_qfit_protein_3mer_trp_2conf_p21(self):
+        """
+        Build a Trp residue with two rotamers
+        """
+        pdb_multi = self._get_file_path("AWA_2conf.pdb")
+        pdb_single = self._get_file_path("AWA_single.pdb")
+        rotamers = self._run_and_validate_identical_rotamers(pdb_multi, pdb_single, d_min=1.2)
+        # this should not find a third distinct conformation (although it may
+        # have overlapped conformations of the same rotamer)
+        assert len(rotamers) == 2
+
+    def test_qfit_protein_3mer_trp_3conf_p21(self):
+        """
+        Build a Trp residue with three different rotamers, two of them
+        with overlapped 5-member rings
+        """
+        pdb_multi = self._get_file_path("AWA_3conf.pdb")
+        pdb_single = self._get_file_path("AWA_single.pdb")
+        rotamers = self._run_and_validate_identical_rotamers(pdb_multi, pdb_single, d_min=1.0)
+        assert len(rotamers) == 3
+        s = Structure.fromfile("multiconformer_model2.pdb")
+        trp_confs = [r for r in s.residues if r.resn[0] == "TRP"]
+        # at 1.0A we should have exactly 3 conformers
+        assert len(trp_confs) == 3
+
+    def _validate_phe_3mer_confs(self, pdb_file_multi):
         rotamers_in = self._get_model_rotamers(pdb_file_multi)
-        rotamers_out = self._get_model_rotamers("multiconformer_model2.pdb")
+        rotamers_out = self._get_model_rotamers("multiconformer_model2.pdb",
+                                                chi_radius=15)
         # Phe2 should have two rotamers, but this may occasionally appear as
         # three due to the ring flips, and we can't depend on which orientation
         # the ring ends up in
         assert (-177, 80) in rotamers_out[2]  # this doesn't flip???
         assert (-65, -85) in rotamers_out[2] or (-65, 85) in rotamers_out[2]
 
-    def test_cli_phe_3mer_p21(self):
+    def test_qfit_protein_3mer_phe_p21(self):
+        """
+        Build a Phe residue with two conformers in P21 at medium resolution
+        """
         d_min = 1.5
-        self._run_qfit_cli(self.PDB_3MER_MULTI,
-                           self.PDB_3MER_SINGLE,
+        (pdb_multi, pdb_single) = self._get_start_models("AFA")
+        self._run_qfit_cli(pdb_multi,
+                           pdb_single,
                            high_resolution=d_min)
-        self._validate_3mer_confs(self.PDB_3MER_MULTI)
+        self._validate_phe_3mer_confs(pdb_multi)
         self._validate_new_fmodel(high_resolution=d_min)
 
-    def test_cli_phe_3mer_p1(self):
+    def test_qfit_protein_3mer_phe_p1(self):
+        """
+        Build a Phe residue with two conformers in a smaller P1 cell at
+        medium resolution
+        """
         d_min = 1.5
-        self._swap_symm(
+        (pdb_multi, pdb_single) = self._get_start_models("AFA")
+        (pdb_multi, pdb_single) = self._replace_symmetry(
             space_group_symbol="P1",
             unit_cell=(12, 6, 10, 90, 105, 90),
-            pdb_multi=self.PDB_3MER_MULTI,
-            pdb_single=self.PDB_3MER_SINGLE)
-        self._run_qfit_cli("multi_p1.pdb",
-                           "single_p1.pdb",
-                           high_resolution=d_min)
-        self._validate_3mer_confs("multi_p1.pdb")
+            pdb_multi=pdb_multi,
+            pdb_single=pdb_single)
+        self._run_qfit_cli(pdb_multi, pdb_single, high_resolution=d_min),
+        self._validate_phe_3mer_confs(pdb_multi)
         self._validate_new_fmodel(high_resolution=d_min)
+
+    def test_qfit_protein_7mer_peptide_p21(self):
+        """
+        Build a 7-mer peptide with multiple residues in double conformations
+        """
+        d_min = 1.3
+        (pdb_multi, pdb_single) = self._get_start_models("GNNAFNS")
+        self._run_qfit_cli(pdb_multi, pdb_single, high_resolution=d_min)
+        self._validate_7mer_confs(pdb_multi)
+        self._validate_new_fmodel(d_min, 0.95)
+
+    def test_qfit_protein_7mer_peptide_p1(self):
+        """
+        Build a 7-mer peptide with multiple residues in double conformations
+        in a smaller P1 cell.
+        """
+        d_min = 1.3
+        (pdb_multi, pdb_single) = self._get_start_models("GNNAFNS")
+        (pdb_multi, pdb_single) = self._replace_symmetry(
+            space_group_symbol="P1",
+            unit_cell=(30, 10, 15, 90, 105, 90),
+            pdb_multi=pdb_multi,
+            pdb_single=pdb_single)
+        self._run_qfit_cli(pdb_multi, pdb_single, high_resolution=d_min)
+        self._validate_7mer_confs(pdb_multi)
+        self._validate_new_fmodel(d_min, 0.95)
+
+    def _validate_7mer_confs(self, pdb_file_multi):
+        rotamers_in = self._get_model_rotamers(pdb_file_multi)
+        rotamers_out = self._get_model_rotamers("multiconformer_model2.pdb",
+                                                chi_radius=15)
+        # Phe5 should have two rotamers, but this may occasionally appear as
+        # three due to the ring flips, and we can't depend on which orientation
+        # the ring ends up in
+        assert (-177, 80) in rotamers_out[5]  # this doesn't flip???
+        assert (-65, -85) in rotamers_out[5] or (-65, 85) in rotamers_out[5]
+        # Asn are also awkward because of flips
+        assert len(rotamers_out[3]) >= 2
+        assert len(rotamers_out[6]) >= 2
+        # these are all of the alt confs present in the fmodel structure
+        assert rotamers_in[3] - rotamers_out[3] == set()
+        assert rotamers_in[2] - rotamers_out[2] == set()
