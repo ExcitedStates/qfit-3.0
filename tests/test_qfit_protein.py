8cbbed75428c17330e0a6711a08a9d90d8ebfdb6
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-08-03 15:48:48 -0700

    updating which 3K0N pdb to choose for test

--- tests/test_qfit_protein.py
+++ tests/test_qfit_protein.py
@@ -33,7 +33,7 @@ class TestQFitProtein:
         # Prepare args
         args = [
             "./example/3K0N.mtz",  # mapfile, using relative directory from tests/
-            "./example/3K0N.pdb",  # structurefile, using relative directory from tests/
+            "./example/3K0N_refine.pdb",  # structurefile, using relative directory from tests/
             "-l", "2FOFCWT,PH2FOFCWT",
         ]
 
