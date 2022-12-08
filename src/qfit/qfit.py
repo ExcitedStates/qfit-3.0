5758d5eb5779ba1d974a26497465d372faf2563f
Daniel Hogan <github@dhogan.io>
2022-12-08 08:59:43 -0800

    remove amplitude randomization

--- src/qfit/qfit.py
+++ src/qfit/qfit.py
@@ -828,8 +828,7 @@ class QFitRotamericResidue(_BaseQFit):
         start_coor = atom.coor[0]  # We are working on a single atom.
         torsion_solutions = []
         for amplitude, direction in itertools.product(amplitudes, directions):
-            delta = self.prng.uniform(-sigma, sigma)
-            endpoint = start_coor + (amplitude + delta) * direction
+            endpoint = start_coor + amplitude * direction
             optimize_result = optimizer.optimize(atom_name, endpoint)
             torsion_solutions.append(optimize_result["x"])
 
@@ -1944,14 +1943,13 @@ class QFitCovalentLigand(_BaseQFit):
             self.options.sample_backbone_amplitude + 0.01,
             self.options.sample_backbone_step,
         )
-        sigma = self.options.sample_backbone_sigma
 
         for amplitude, direction in itertools.product(amplitudes, directions):
-            endpoint = start_coor + (amplitude + sigma * self.prng.random()) * direction
+            endpoint = start_coor + amplitude * direction
             optimize_result = optimizer.optimize(atom_name, endpoint)
             torsion_solutions.append(optimize_result["x"])
 
-            endpoint = start_coor - (amplitude + sigma * self.prng.random()) * direction
+            endpoint = start_coor - amplitude * direction
             optimize_result = optimizer.optimize(atom_name, endpoint)
             torsion_solutions.append(optimize_result["x"])
 
