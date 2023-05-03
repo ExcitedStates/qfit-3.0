1aa2d4a1d22403ebe08e801a9c9bd17019c3cc6d
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2023-05-03 12:21:31 -0700

    adding EM option to residue

--- src/qfit/qfit_residue.py
+++ src/qfit/qfit_residue.py
@@ -47,6 +47,13 @@ def build_argparser():
         help="Chain, residue id, and optionally insertion code for "
         "residue in structure, e.g. A,105, or A,105:A.",
     )
+    
+    p.add_argument(
+        "-em",
+        "--cryo_em",
+        action="store_false",
+        help="Run qFit with EM options",
+    )
 
     # Map input options
     p.add_argument(
