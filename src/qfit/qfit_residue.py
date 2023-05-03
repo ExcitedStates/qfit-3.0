8d8fd3e039822dce7950a91f2006203b7737fa74
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2023-05-03 14:45:07 -0700

    removing scattering from scaler

--- src/qfit/qfit_residue.py
+++ src/qfit/qfit_residue.py
@@ -452,7 +452,7 @@ def main():
     xmap = xmap.canonical_unit_cell()
     if args.scale:
         # Prepare X-ray map
-        scaler = MapScaler(xmap, scattering=options.scattering, em=options.em)
+        scaler = MapScaler(xmap, em=options.em)
         if args.omit:
             footprint = structure_resi
         else:
