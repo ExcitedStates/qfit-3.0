d632f3b3fbe967fb762c85492713ae3086b8a918
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2023-05-11 15:07:43 -0700

    adding nconf lines if segment

--- src/qfit/qfit.py
+++ src/qfit/qfit.py
@@ -288,6 +288,7 @@ class _BaseQFit:
         threshold,
         loop_range=[0.5, 0.4, 0.33, 0.3, 0.25, 0.2],
         do_BIC_selection=None,
+        segment=None,
     ):
         #set loop range differently for EM
         if self.options.em:
@@ -314,7 +315,8 @@ class _BaseQFit:
                 nconfs = np.sum(solver.weights >= 0.002)
                 model_params_per_atom = 3 + int(self.options.sample_bfactors)
                 k = model_params_per_atom * natoms * nconfs * 0.95 #0.95 hyperparameter in put in here since we are almost always over penalizing 
-
+                if segment is not None:
+                    k = nconfs. #for segment, we only care about the number of conformations come out of MIQP. Considering atoms penalizes this too much
                 BIC = n * np.log(rss / n) + k * np.log(n)
                 solution = MIQPSolutionStats(
                     threshold=threshold,
