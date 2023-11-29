41f682da98c35e967b6111047ccf36f5c4ed0368
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2023-11-28 21:19:50 -0800

    add run qfit water

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ def main():
                 "add_non_rotamer_atoms = qfit.add_non_rotamer_atoms:main",
                 "remove_duplicates = qfit.remove_duplicates:main",
                 "create_rotamer_library = qfit.create_rot_lib:main",
+                "qfit_water = qfit.run_qfit_water:main",
             ]
         },
         scripts=[
