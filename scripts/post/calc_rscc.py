1da887a96d0bb59080c3ba31111b9adac8cd702c
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2024-01-17 10:15:05 -0800

    add comments

--- scripts/post/calc_rscc.py
+++ scripts/post/calc_rscc.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
+'''
+This script will calculate the RSCC of a ligand (or any residue) defined by their ligand name (--ligand) or residue number and chain id (--resi_chain). 
+It will only work on mtz maps with 2FOFCWT,PH2FOFCWT. 
+
+To run: 
+ calc_rscc.py PDB_FILE.pdb MTZ_FILE.mtz --ligand AR6 --pdb PDB_NAME --directory /path/for/output/csv/file
+'''
+
 from argparse import ArgumentParser
 import numpy as np
 import pandas as pd
+from qfit.scaler import MapScaler
 from qfit.structure import Structure
+from qfit.volume import XMap
 from qfit.validator import Validator
+
+
 
 
 def build_argparser():
     p = ArgumentParser(description=__doc__)
     p.add_argument("structure", type=str, help="PDB-file containing structure.")
     p.add_argument("map", type=str, help="Map.")
-    p.add_argument("ligand", type=str, help="name of ligand")
+    p.add_argument("--ligand", type=str, help="name of ligand")
+    
     p.add_argument("--pdb", type=str, help="name of PDB")
-
+    p.add_argument("--directory", type=str, help="Where to save RSCC info")
     return p
 
 
@@ -23,20 +36,32 @@ def main():
   structure = Structure.fromfile(options.structure)
   ligand = structure.extract("resn", options.ligand, "==")
 
-
   # Load and process the electron density map:
   xmap = XMap.fromfile(
-    options.map
+    options.map, label='2FOFCWT,PH2FOFCWT'
   )
+  scaler = MapScaler(xmap)
   xmap = xmap.canonical_unit_cell()
-  if options.scale:
-    footprint = ligand
-    scaler.scale(footprint, radius=options.scale_rmask * radius)
+  footprint = ligand
+  scaler.scale(footprint, radius=1.5)
 
-  xmap = xmap.extract(ligand.coor, padding=11)
-
-
+  xmap = xmap.extract(ligand.coor, padding=8)
 
   # Now that the conformers have been generated, the resulting
   # # conformations should be examined via GoodnessOfFit:
-  validator = Validator(xmap, xmap.resolution)
+  validator = Validator(xmap, xmap.resolution, options.directory)
+  rscc = validator.rscc(ligand)
+  print("rscc = ", rscc)
+
+  csv_filename = f"{options.pdb}_rscc.csv"
+
+  # Write to CSV
+  with open(csv_filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    # Write the header
+    writer.writerow(["PDB", "RSCC"])
+    # Write the data
+    writer.writerow([options.pdb, rscc])
+
+if __name__ == "__main__":
+    main()
