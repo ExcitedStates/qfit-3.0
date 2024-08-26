5084a7964c2b754a662745a5d5b050bdf4057c8b
Stephanie (Mullane) Wankowicz <mullane.stephanie@gmail.com>
2024-08-26 08:51:44 -0700

    Update lig_occ.py

--- scripts/post/lig_occ.py
+++ scripts/post/lig_occ.py
@@ -20,12 +20,13 @@ def parse_args():
     p.add_argument("structure", type=str, help="PDB-file containing structure.")
     p.add_argument("-l", "--ligand")
     p.add_argument("--pdb", help="Name of the input PDB.")
+    p.add_argument("--qFit", help="qFit included in output file name")
 
     args = p.parse_args()
     return args
 
 
-def get_occ(structure, ligand, pdb):
+def get_occ(structure, ligand, pdb, qfit):
     lig_str = structure.extract("resn", ligand, "==")
     lig = []
     for i in np.unique(lig_str.chain):
@@ -45,15 +46,19 @@ def get_occ(structure, ligand, pdb):
         lig,
         columns=["PDB", "ligand_name", "chain", "min_occ", "average_b", "num_altloc"],
     )
-    occ.to_csv(pdb + "_ligand_occupancy.csv", index=False)
+    occ.to_csv(pdb + qfit + "_ligand_occupancy.csv", index=False)
 
 
 def main():
     args = parse_args()
+    if not args.qFit == None:
+        qfit = _qFit
+    else:
+        qfit = ''
     # Load structure and prepare it
     structure = Structure.fromfile(args.structure).reorder()
     structure = structure.extract("e", "H", "!=")
-    get_occ(structure, args.ligand, args.pdb)
+    get_occ(structure, args.ligand, args.pdb, qfit)
 
 
 if __name__ == "__main__":
