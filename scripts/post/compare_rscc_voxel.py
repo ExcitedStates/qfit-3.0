23686cf00f0c82e5a27462ea8c45885a4ed3161a
Stephanie (Mullane) Wankowicz <mullane.stephanie@gmail.com>
2025-08-14 13:51:56 -0500

    update to compare against the same map

--- scripts/post/compare_rscc_voxel.py
+++ scripts/post/compare_rscc_voxel.py
@@ -14,11 +14,9 @@ This script is based on calc_rscc.py and is designed to compare the RSCC (Real-S
 It calculates the RSCC for a ligand (or any residue) defined by its ligand name (--ligand) or by its residue number and chain ID (--residue). The script takes two ligands, 
 with their corresponding electron density maps, as input and calculates the RSCC of each ligand within the same voxel space, ensuring a proper comparison between the deposited and qFit-generated models.
 
-Note: This script only works on MTZ maps containing 2FOFCWT,PH2FOFCWT map labels.
-
 
 To run: 
- compare_rscc_voxel PDB_FILE_deposited.pdb MTZ_FILE_deposited.mtz --gen_pdb PDB_FILE_QFIT.pdb --gen_map MTZ_FILE_QFIT.mtz --residue A,401 --pdb PDB_NAME --directory /path/for/output/csv/file
+ compare_rscc_voxel PDB_FILE_deposited.pdb MTZ_FILE_deposited.mtz --gen_pdb PDB_FILE_QFIT.pdb --residue A,401 --pdb PDB_NAME --directory /path/for/output/csv/file
 
 """
 
@@ -26,19 +24,22 @@ To run:
 def build_argparser():
     p = ArgumentParser(description=__doc__)
     p.add_argument(
-        "structure", type=str, help="deposited PDB-file containing structure."
+        "base_structure", type=str, help="Base PDB-file containing structure."
     )
-    p.add_argument("map", type=str, help="deposited map.")
-    p.add_argument("--gen_pdb", type=str, help="qFit generated pdb file")
-    p.add_argument("--gen_map", type=str, help="qFit generated map file")
-    p.add_argument(
-        "--ligand", type=str, help="name of ligand for RSCC to be calculated on"
-    )
+    p.add_argument("map", type=str, help="X-ray density map in CCP4 or MRC or MTZ file")
+    p.add_argument("--comp_pdb", type=str, help="qFit generated pdb file")
     p.add_argument(
         "--residue", type=str, help="Chain_ID, Residue_ID for RSCC to be calculated on"
     )
+    p.add_argument(
+        "-l",
+        "--label",
+        default="2FOFCWT,PH2FOFCWT",
+        metavar="<F,PHI>",
+        help="MTZ column labels to build density. Required if MTZ format",
+    )
     p.add_argument("--pdb", type=str, help="name of PDB")
-    p.add_argument("--directory", type=str, help="Where to save RSCC info")
+    p.add_argument("--directory", type=str, default="", help="Where to save RSCC info")
     return p
 
 
@@ -46,22 +47,22 @@ def main():
     p = build_argparser()
     options = p.parse_args()
     # Load structure and prepare it
-    dep_structure = Structure.fromfile(options.structure)
-    gen_strucutre = Structure.fromfile(options.gen_pdb)
+    dep_structure = Structure.fromfile(options.base_structure)
+    gen_strucutre = Structure.fromfile(options.comp_pdb)
 
     # Get the generated and deposited ligand coordinates
-    if options.ligand is not None:
-        gen_ligand = gen_strucutre.extract("resn", options.ligand, "==")
-        dep_ligand = dep_structure.extract("resn", options.ligand, "==")
-    elif options.residue is not None:
+    if options.residue is not None:
         chainid, resi = options.residue.split(",")
         gen_ligand = gen_strucutre.extract(f"resi {resi} and chain {chainid}")
         dep_ligand = dep_structure.extract(f"resi {resi} and chain {chainid}")
     else:
-        print("Please provide ligand name or residue ID and chain ID")
+        print("Please provide residue name or residue ID and chain ID")
+
+    # Add the deposited ligand to the combined structure
+    combined_structure = gen_ligand.combine(dep_ligand)
 
     # Load and process the electron density maps:
-    dep_xmap = XMap.fromfile(options.map, label="2FOFCWT,PH2FOFCWT")
+    dep_xmap = XMap.fromfile(options.map, label=options.label)
     dep_scaler = MapScaler(dep_xmap)
     dep_xmap = dep_xmap.canonical_unit_cell()
     footprint = gen_ligand  # set voxel space around the generated ligand
@@ -71,35 +72,13 @@ def main():
         gen_ligand.coor, padding=8
     )  # Create a copy of the deposited map around the atomic coordinates provided.
 
-    gen_xmap = XMap.fromfile(options.gen_map, label="2FOFCWT,PH2FOFCWT")
-    gen_scaler = MapScaler(gen_xmap)
-    gen_xmap = gen_xmap.canonical_unit_cell()
-    footprint = gen_ligand  # set voxel space around the generated ligand
-    gen_scaler.scale(footprint, radius=1.5)
-
-    gen_xmap = gen_xmap.extract(
-        gen_ligand.coor, padding=8
-    )  # Create a copy of the map around the atomic coordinates provided.
-
-    ext = ".ccp4"
-    scaled_fname = os.path.join(
-        options.directory, f"dep{ext}"
-    )  # this should be an option
-
-    dep_xmap.tofile(scaled_fname)
-
-    gen_fname = os.path.join(options.directory, f"gen{ext}")  # this should be an option
-
-    gen_xmap.tofile(gen_fname)
-
     # Now that the conformers have been generated, the resulting
     # # conformations should be examined via GoodnessOfFit:
     dep_validator = Validator(dep_xmap, dep_xmap.resolution, options.directory)
     dep_rscc = dep_validator.rscc(dep_ligand)
     print(dep_rscc)
 
-    gen_validator = Validator(gen_xmap, gen_xmap.resolution, options.directory)
-    gen_rscc = gen_validator.rscc(gen_ligand)
+    gen_rscc = dep_validator.rscc(gen_ligand)
     print(gen_rscc)
 
     csv_filename = f"{options.pdb}_rscc.csv"
